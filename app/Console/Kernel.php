8f6a536ce1ab9a4d8734a848325c53259653135e
Ivan Atanasov <atanassoff.i@gmail.com>
2016-05-17 21:44:08 +0200

    add base functionality from tutorial

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+         Commands\Inspire::class,
+         Commands\CacheApi::class,
     ];
 
     /**
---
+++ config/marvel.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'private_key' => env('MARVEL_PRIVATE_KEY'),
+    'public_key'  => '382e7fe7375556c9c88f6453ca21bd72',
+    'base_uri'    => 'http://gateway.marvel.com/v1/public/',
+];
