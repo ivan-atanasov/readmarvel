ea3881e463e9ce18d7853a923f77264bcbcbf34b
Ivan Atanasov <atanassoff.i@gmail.com>
2016-05-26 16:21:32 +0200

    move static texts to lang files

---
+++ resources/lang/en/frontend/navbar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title'      => 'Marvel Viewer',
+    'home'       => 'Home',
+    'comics'     => 'Comics',
+    'characters' => 'Characters',
+    'login'      => 'Login',
+    'logout'     => 'Logout',
+    'register'   => 'Register',
+];
