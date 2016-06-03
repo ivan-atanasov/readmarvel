93c41815dd9f7ed79d87fe54d57b6cfa78c250d0
Ivan Atanasov <atanassoff.i@gmail.com>
2016-06-04 01:08:09 +0200

    add form to add an item on a list; add mgirations; add datepicker;

---
+++ app/Entities/MarvelListItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entities;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class MarvelListItem
+ * @package App\Entities
+ */
+class MarvelListItem extends Model
+{
+    /** @var array */
+    protected $fillable = [
+        'list_id',
+        'comic_id',
+        'score',
+        'rewatch_value',
+        'progress',
+        'comment',
+        'started_at',
+        'finished_at'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function marvelList()
+    {
+        return $this->belongsTo('App\\Entities\\MarvelList', 'list_id');
+    }
+}
