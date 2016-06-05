81d3b6791b455907c65354198253880134173110
Ivan Atanasov <atanassoff.i@gmail.com>
2016-06-05 12:10:00 +0200

    add basic design for user profile

--- resources/lang/en/frontend/profile.php
+++ resources/lang/en/frontend/profile.php
@@ -1,14 +1,16 @@
 <?php
 
 return [
-    'real_name'     => 'Real name',
-    'about_me'      => 'About me',
-    'avatar'        => 'Avatar',
-    'submit'        => 'Update profile',
-    'validations'   => [
+    'real_name'                => 'Real name',
+    'about_me'                 => 'About me',
+    'avatar'                   => 'Avatar',
+    'submit'                   => 'Update profile',
+    'validations'              => [
         'real_name_regex' => 'Your name can contain only letters and spaces',
         'about_me_max'    => 'About me can be maximum 140 characters',
     ],
-    'personal_info' => 'Personal info',
-    'manage_lists'  => 'Manage lists',
+    'personal_info'            => 'Personal info',
+    'manage_lists'             => 'Manage lists',
+    'add_new_list'             => 'Add a new list',
+    'add_new_list_description' => 'Click the gray plus above to add a new list to your collections',
 ];
