f4272f758daadf9508f39ebcbd9f16ca11bb4750
Ivan Atanasov <atanassoff.i@gmail.com>
2016-06-28 18:54:47 +0200

    add forgotten password functionality

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          Commands\Inspire::class,
-         Commands\CacheApi::class,
          Commands\GenerateDefaultLists::class,
     ];
 
