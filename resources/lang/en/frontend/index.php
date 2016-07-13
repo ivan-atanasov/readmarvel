a7f461264685e1cfcff14c74d78f73c207f7ad5d
Ivan Atanasov <atanassoff.i@gmail.com>
2016-07-13 21:23:53 +0200

    M-3: add public url share button

--- resources/lang/en/frontend/index.php
+++ resources/lang/en/frontend/index.php
@@ -6,4 +6,5 @@ return [
     'confirm_delete_body'  => 'Are you sure you want to delete this record?',
     'close'                => 'Close',
     'confirm'              => 'Confirm',
+    'copy'                 => 'Copy',
 ];
