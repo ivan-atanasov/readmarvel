bd306fa3fbd0c0e6bb99b48d53d9d492081efcd2
Ivan Atanasov <atanassoff.i@gmail.com>
2016-07-13 23:44:50 +0200

    M-2: add comments migration; add comment entity; add routes; add comment form in series page;

---
+++ app/Http/Controllers/Frontend/CommentsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Requests\CommentRequest;
+
+/**
+ * Class CommentsController
+ * @package App\Http\Controllers\Frontend
+ */
+class CommentsController extends BaseController
+{
+    public function store(CommentRequest $request)
+    {
+
+    }
+}
