abaaecbc313fb661657e775f0e92319f0eefe88f
Ivan Atanasov <atanassoff.i@gmail.com>
2016-08-06 14:04:44 +0200

    M-12: add editing / deleting of records; add debugbar; add footer list in FE;

--- app/Http/Controllers/Admin/StaticPagesController.php
+++ app/Http/Controllers/Admin/StaticPagesController.php
@@ -63,18 +63,6 @@ class StaticPagesController extends HomeController
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param $id
