63c9009da6582212e0c3b9a731587474e4d591b8
Ivan Atanasov <atanassoff.i@gmail.com>
2016-08-11 23:19:00 +0200

    M-17: fix public list not showing images;

--- app/Http/Controllers/Frontend/PublicListsController.php
+++ app/Http/Controllers/Frontend/PublicListsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Entities\MarvelList;
+use App\Helpers\ImageHelper;
 use App\Repositories\MarvelListRepository;
 use View;
 
@@ -27,12 +29,38 @@ class PublicListsController extends BaseController
     /**
      * @param string $hash
      *
-     * @return $this
+     * @return \Illuminate\Contracts\View\View
      */
     public function show(string $hash)
     {
-        $list = $this->marvelListRepository->findByHash($hash);
+        $list = [$this->marvelListRepository->findByHash($hash)];
+        $this->getListsAvatars($list);
+        $list = array_pop($list);
 
-        return View::make('frontend.lists.list')->with('list', $list);
+        return View::make('frontend.lists.list')->with('list', $list)->with('public', true);
+    }
+
+    /**
+     * @param array $lists
+     */
+    private function getListsAvatars(array &$lists)
+    {
+        /**
+         * @var int   $key
+         * @var array $list
+         */
+        foreach ($lists as $key => $list) {
+            $lists[$key]['avatar'] = '';
+            $avatar = ImageHelper::path(
+                MarvelList::IMAGE_RESOURCE,
+                $list['id'],
+                ImageHelper::SMALL,
+                $list['avatar']
+            );
+
+            if (file_exists(public_path() . $avatar)) {
+                $lists[$key]['avatar'] = $avatar;
+            }
+        }
     }
 }
