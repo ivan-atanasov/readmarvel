dd5b73faed07440e0e07795e9d5ea788e51fabcb
Ivan Atanasov <atanassoff.i@gmail.com>
2016-08-28 16:22:21 +0200

    32: add footer image; fix forgotten pass; fix add to list;

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends BaseController
             $m->to(Config::get('mail.contact_form_to_email'), $data['name'])->subject($data['subject']);
         });
 
-        Session::put('messages', ['success' => 'Message sent']);
+        Session::flash('messages', ['success' => 'Message sent']);
         return Redirect::back();
     }
 }
