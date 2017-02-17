bc6687bf4d5d9bd001399f2f5a12e451c68c201c
Ivan Atanasov <ivan.j.atanasov@gmail.com>
2017-02-18 00:00:36 +0100

    M59: fix profile page;

--- resources/assets/js/frontend/profile.js
+++ resources/assets/js/frontend/profile.js
@@ -2,3 +2,14 @@ $('#upload-avatar').change(function () {
     // select the form and submit
     $('#upload-avatar-form').submit();
 });
+
+$(document).ready(function() {
+    $('button[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        e.preventDefault();
+
+        $('button[data-toggle="tab"]').removeClass('selected');
+        var target = $(e.target);
+        target.addClass('selected');
+        // alert(target);
+    });
+});
