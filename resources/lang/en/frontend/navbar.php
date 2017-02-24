ee5e157a3b0eb87c18cd3a6cc385028c753630fd
Ivan Atanasov <ivan.j.atanasov@gmail.com>
2017-02-25 00:00:51 +0100

    M59: add feedback link in footer

--- resources/lang/en/frontend/navbar.php
+++ resources/lang/en/frontend/navbar.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-    'title'      => 'Read Marvel.com - Create you Marvel comic books lists',
-    'home'       => 'Home',
-    'comics'     => 'Comics',
-    'characters' => 'Characters',
-    'login'      => 'Login',
-    'logout'     => 'Logout',
-    'register'   => 'Register',
-    'profile'    => 'Profile',
-    'series'     => 'Series',
-    'contact'    => 'Contact us',
+    'title'            => 'Read Marvel.com - Create you Marvel comic books lists',
+    'home'             => 'Home',
+    'comics'           => 'Comics',
+    'characters'       => 'Characters',
+    'login'            => 'Login',
+    'logout'           => 'Logout',
+    'register'         => 'Register',
+    'profile'          => 'Profile',
+    'series'           => 'Series',
+    'contact'          => 'Contact us',
+    'give_us_feedback' => 'Give us feedback',
 ];
