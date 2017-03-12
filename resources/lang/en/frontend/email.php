4b680521b44fadd59ed59b28f375d32cb5065132
Ivan Atanasov <ivan.j.atanasov@gmail.com>
2017-03-12 23:52:19 +0100

    Fix: subject lines

--- resources/lang/en/frontend/email.php
+++ resources/lang/en/frontend/email.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'subject_welcome'  => 'Thank you for joining ReadMarvel.com',
-    'subject_followup' => 'Followup ReadMarvel.com',
+    'subject_welcome'  => 'Welcome to Read Marvel',
+    'subject_followup' => 'How to create your comic book lists',
 ];
